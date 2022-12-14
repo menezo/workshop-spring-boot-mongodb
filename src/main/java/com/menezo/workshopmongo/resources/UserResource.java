5bd1f8c88a2d0965d9719b5e052ac0a8bfe23b42
Menezo <joaovitormenezesm12@gmail.com>
2022-12-14 11:46:48 -0300

    Endpoint for the Posts of an user

--- src/main/java/com/menezo/workshopmongo/resources/UserResource.java
+++ src/main/java/com/menezo/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.menezo.workshopmongo.domain.Post;
 import com.menezo.workshopmongo.domain.User;
 import com.menezo.workshopmongo.dtos.UserDTO;
 import com.menezo.workshopmongo.services.UserService;
@@ -62,5 +63,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
